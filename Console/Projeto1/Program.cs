d1c00e6c488e9cf8467c26eb769a25dd022bc2fd
alves1609 <isabella.candido@aluno.faculdadeimpacta.com.br>
2020-11-17 11:35:41 -0300

    Projeto console feito

---
+++ Console/Projeto1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro numero:");
+            string num1_texto = Console.ReadLine();
+            int num1 = int.Parse(num1_texto);
+
+            Console.Write("Digite o segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+
+            Console.Write("O resultato do " +num1+ "+" +num2+ "é:" +soma);
+            
+        }
+    }
+}
