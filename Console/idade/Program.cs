c82f7fb2acd8e13a6e7c0a3f0fe1fc5c0388ea38
alves1609 <isabella.candido@aluno.faculdadeimpacta.com.br>
2020-11-17 18:02:08 -0300

    Exercicio idade concluido

---
+++ Console/idade/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("idade");
+
+            Console.WriteLine("Bem vindo! Em que ano você nasceu?");
+            int nascimento =  int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Em que anos estamos?");
+            int ano = int.Parse(Console.ReadLine());
+
+            int resultado =  ano - nascimento;
+            Console.WriteLine("Sua idade é de " + resultado + " anos");
+
+            int semanas = (nascimento * 365) / 7;
+            Console.WriteLine("E sua idade convertida em semanas da um total de: " + semanas);
+        }
+    }
+}
+    
