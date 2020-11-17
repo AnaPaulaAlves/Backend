ad75dd1e3f691ab0c788d1f5a3c6e3244ddbabad
alves1609 <isabella.candido@aluno.faculdadeimpacta.com.br>
2020-11-17 12:34:17 -0300

    Projeto concluido

---
+++ Console/media/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Digite a primeira nota: ");
+           double nota1 = double.Parse(Console.ReadLine());
+
+           Console.Write("Digite a segunda nota: ");
+           double nota2 = double.Parse(Console.ReadLine());
+     
+
+           double media = (nota1 + nota2) / 2;
+
+           string situacao = "";
+
+           Console.WriteLine("Média:  " + media);
+
+           if(media > 7) {
+               situacao = "Aprovado";
+           }else{
+               situacao = "Reprovado";
+           }
+
+           Console.WriteLine(situacao);
+        }
+    }
+}
