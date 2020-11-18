3595e4d3b9cf962f03dedd274536577562e5c097
alves1609 <isabella.candido@aluno.faculdadeimpacta.com.br>
2020-11-18 13:45:15 -0300

    Laços while, for

---
+++ Console/Projeto2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+     
+     {
+         Console.Write("Você gostaria de receber bom dia?");
+         string resposta = Console.ReadLine();
+
+            while(resposta == "sim"){ // true quando a condição for verdade
+            Console.WriteLine("Bom dia");
+
+            Console.WriteLine("Você ainda deseja receber um bom dia?");
+            resposta = Console.ReadLine();
+
+            Console.WriteLine("fim");
+
+            }
+        }
+    }
+}
