d533cd4d3c148468547480ca91e51779ed6428bc
alves1609 <isabella.candido@aluno.faculdadeimpacta.com.br>
2020-11-18 13:04:05 -0300

    pasta semaforo

---
+++ Console/Semaforo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Transito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Semaforo cor");
+            string Semaforo = (Console.ReadLine());
+
+            if(Semaforo == "green"){
+                Console.WriteLine("semaforo verde anda");
+            }else if(Semaforo == "wellow"){
+                Console.WriteLine("Diminua a velocidade do veiculos");
+            }else if(Semaforo == "red"){
+                Console.WriteLine("Pare o veiculo");
+            
+            }    
+            
+            }
+    }
+}
+     
